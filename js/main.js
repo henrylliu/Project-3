a64f9a3cd3cf435826508881e817b54be2142af9
Henry Liu <hel044@ucsd.edu>
2025-11-11 17:57:35 -0800

    Added toy example of graph

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 import { createTimeSlider } from "./slider.js";
 import { createSouthAmericaMap } from "./southAmericaMap.js";
-
-
-
+const alpha3ToNum = {
+    "ARG": "032",
+    "BOL": "068",
+    "BRA": "076",
+    "CHL": "152",
+    "COL": "170",
+    "ECU": "218",
+    "GUY": "328",
+    "PRY": "600",
+    "PER": "604",
+    "SUR": "740",
+    "URY": "858",
+    "VEN": "862"
+};
 
 const sampleData = [
-    { id: "BRA", value: 8 },
-    { id: "PER", value: 5 },
-    { id: "COL", value: 6 },
-    { id: "ARG", value: 3 }
-  ];
+    { id: "076", value: 8 },
+    { id: "604", value: 5 },
+    { id: "170", value: 6 },
+    { id: "032", value: 3 }
+];
   
-  createSouthAmericaMap(sampleData, "#map-container");
+createSouthAmericaMap(sampleData, "#map-container");
 
 createTimeSlider({
     parentSelector: "#slider-container",
